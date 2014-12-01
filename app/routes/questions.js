2246ca14bf8d4944adc81a6b774076bd6cb0bd45
aodysseos <andreasodysseos91@gmail.com>
2014-12-01 10:34:38 +0000

    Questions single interface

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -6,40 +6,19 @@ router.get('/questions', function(req, res) {
 	req.models.question.find(
 			{}, // search parameters: anything 
 			{}, // no options
-			function (err, all_questions) {
+			function (err, question_list) {
 				if (err) {
 					console.log('error!', err);
 					return;
 				}
 				// send to the client what we found in the DB
-				res.send(JSON.stringify(all_questions));
+				res.render('questions', JSON.stringify(question_list));
+				//res.send(JSON.stringify(all_questions));
 			});
 });
 
-/* GET form add a question */
-router.get('/questions/add', function (req, res) {
-	 res.render('add', { title: "Add a question" });
-});
-
-/* GET single question. */
-router.get('/questions/:id', function(req, res) {
-	var questions = [
-	    {title:"Name", content:"What is your name?"}, 
-	    {title:"Surname", content:"What is your surname?"}, 
-	    {title:"Study", content: "What do you study?"}
-	];
-
-  	if(questions.length <= req.params.id || req.params.id < 0) {
-	    res.statusCode = 404;
-	    res.render('questions', { title: "Error 404", content: "No question found" });
-  	}  
-  	
-  	var q = questions[req.params.id];
- 	res.render('questions', { title: q.title, content: q.content });
-});
-
-/* POST add a question */
-router.post('/questions/add', function (req, res) {
+/* POST a question */
+router.post('/questions', function (req, res) {
 	var q_title = req.body.title;
 	var q_content = req.body.content;
 	// Note: Needs to validate the data
@@ -51,6 +30,16 @@ router.post('/questions/add', function (req, res) {
 			function (err, questions_created) {
 				res.send(JSON.stringify(questions_created));
 			});
+});
+
+/* GET single question. */
+router.get('/questions/:id', function(req, res) {
+  	if(questions.length <= req.params.id || req.params.id < 0) {
+	    res.statusCode = 404;
+	    res.render('questions', { title: "Error 404", content: "No question found" });
+  	}  
+  	var q = questions[req.params.id];
+ 	res.render('questions', { title: q.title, content: q.content });
 });
 
 module.exports = router;
