e3d7e8a34523944300ee0d4d8a8f2683e81e9686
Miłosz Gaczkowski <milosz@omgomg.eu>
2014-12-04 01:08:59 +0000

    Update database schema to correctly reflect coursework spec
    Each answer can belong to at most 1 question, but each question can have
    many answers. Each question and answer can have multiple comments, but
    any given comment can only belong to one question/answer.

--- app/models/database.js
+++ app/models/database.js
@@ -3,22 +3,27 @@ var orm = require('orm');
 module.exports.connectionString = "sqlite://store.sqlite3";
 
 module.exports.define = function(db, models) {
-  	models.question = db.define('question', {
-            title: String,
-            content: String
-        });
+	models.question = db.define('question', {
+		title: String,
+		content: String
+	});
 
-  	models.comment = db.define('answer', {
-            title: String,
-            content: String
-        });
+	models.answer = db.define('answer', {
+		title: String,
+		content: String
+	});
+	
+	models.q_comment = db.define('q_comment', {
+		title: String,
+		content: String
+	});
+	
+	models.a_comment = db.define('a_comment', {
+		title: String,
+		content: String
+	});
 
-  	models.answer = db.define('comment', {
-            title: String,
-            content: String
-        });
-
-    models.question.hasOne("answer", db.models.answer, { reverse: 'question' });
-    models.question.hasOne('comment',db.models.comment, {reverse: 'question'});
-    models.answer.hasOne('comment',db.models.comment, {reverse: 'answer'});
+		models.answer.hasOne('Question', db.models.question, {reverse: 'Answers'});
+		models.q_comment.hasOne('Question', db.models.question, {reverse: 'Comments'});
+		models.a_comment.hasOne('Answer', db.models.answer, {reverse: 'Comments'});
 };
