3379fd4fa9507fa1475a5d767c48f7ab9e4b429a
Miłosz Gaczkowski <milosz@omgomg.eu>
2014-12-08 10:11:56 +0000

    Dependencies

--- app/app.js
+++ app/app.js
@@ -4,9 +4,8 @@ var orm = require('orm');
 var swig  = require('swig');
 var methodOverride = require('method-override');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var slashes = require('connect-slashes');
 
 var database = require('./models/database');
 
@@ -19,15 +18,14 @@ var a_comments = require('./routes/a_comments');
 var app = express();
 
 // view engine setup
-app.engine('html', swig.renderFile); // render html with swing
+app.engine('html', swig.renderFile); // render html with swig
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
 
 //app.enable('strict routing');
 app.use(orm.express(database.connectionString));
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(slashes(false));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
