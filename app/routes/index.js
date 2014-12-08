856d20be41d10a92afcc07ece00636fa76440224
Miłosz Gaczkowski <milosz@omgomg.eu>
2014-12-08 09:15:41 +0000

    Home page

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express', text: 'Welcome to my RESTapp' });
+    if (req.accepts('html', 'json') === false) {
+        res.status(406).send("Not Acceptable. This application supports text/html and application/json responses.\n\n");
+        return;
+    }
+    if (req.accepts('html', 'json') === 'json') {
+        res.type('json');
+        res.send(JSON.stringify({online: true}));
+    } else {
+        res.render('index');
+    }
 });
 
 module.exports = router;
